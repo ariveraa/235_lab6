97eb5dd042eabf3effbaeb65a1c0ab0ae17119cb
Adrian Rivera <riveracavana@gmail.com>
2018-11-08 11:35:19 -0700

    first

---
+++ cmake-build-debug/CMakeFiles/lab6_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6_.dir/main.cpp.o"
+  "lab6_.pdb"
+  "lab6_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
